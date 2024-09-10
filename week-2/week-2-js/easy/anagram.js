269c7e266893d506968b8063cb33cf69fefd6822
Pankaj Singh <pankrj123@gmail.com>
2024-09-10 06:39:36 +0000

    solved one assignment

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -6,6 +6,11 @@
 
 function isAnagram(str1, str2) {
 
+  let str1_low = str1.toLowerCase();
+  let str2_low = str2.toLowerCase();
+  let s = str1_low.split("").sort().join("");
+  let s2 = str2_low.split("").sort().join("");
+  return s==s2;
 }
 
 module.exports = isAnagram;
